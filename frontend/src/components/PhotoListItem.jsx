12afe6bbc839e4657f300632130a710a6eb44d35
sfia-o <sofia.vl.oliveira@gmail.com>
2023-09-12 16:14:47 -0700

    Add classNames and styling

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -6,11 +6,15 @@ const PhotoListItem = (props) => {
   /* Insert React */
   const { data } = props;
   return(
-  <div className='PhotoListItem'>
-    <img src={data.imageSource} alt="photo" />
-      <img src={data.profile} alt="photographer-profile-pic" />
+  <div className='photo-list__item'>
+    <img className="photo-list__image" src={data.imageSource} alt="photo" />
+    <div className="photo-list__user-details">
+      <img className="photo-list__user-profile" src={data.profile} alt="photographer-profile-pic" />
+      <div className="photo-list__user-info">
       <p>{data.username}</p>
-      <p>{data.location.city}, {data.location.country}</p>
+      <p className="photo-list__user-location">{data.location.city}, {data.location.country}</p>
+      </div>
+    </div>
   </div>
 
   )
