d24335c6a851137fd52cf650de3fed409ff28074
sfia-o <sofia.vl.oliveira@gmail.com>
2023-09-12 15:55:46 -0700

    Refactor PhotoListItem to use props.

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,26 +2,15 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
+const PhotoListItem = (props) => {
   /* Insert React */
+  const { data } = props;
   return(
   <div className='PhotoListItem'>
-    <img src={sampleDataForPhotoListItem.imageSource} alt="photo" />
-      <img src={sampleDataForPhotoListItem.profile} alt="photographer-profile-pic" />
-      <p>{sampleDataForPhotoListItem.username}</p>
-      <p>{sampleDataForPhotoListItem.location.city}, {sampleDataForPhotoListItem.location.country}</p>
+    <img src={data.imageSource} alt="photo" />
+      <img src={data.profile} alt="photographer-profile-pic" />
+      <p>{data.username}</p>
+      <p>{data.location.city}, {data.location.country}</p>
   </div>
 
   )
