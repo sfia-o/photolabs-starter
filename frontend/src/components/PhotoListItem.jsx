068ebc655355443143c4e6613f5383c16a93401e
sfia-o <sofia.vl.oliveira@gmail.com>
2023-09-13 11:58:41 -0700

    Add FavButton to PhotoListItem component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from './PhotoFavButton';
 
 const PhotoListItem = (props) => {
   /* Insert React */
   const { data } = props;
   return(
   <div className='photo-list__item'>
+    <PhotoFavButton />
     <img className="photo-list__image" src={data.imageSource} alt="photo" />
     <div className="photo-list__user-details">
       <img className="photo-list__user-profile" src={data.profile} alt="photographer-profile-pic" />
