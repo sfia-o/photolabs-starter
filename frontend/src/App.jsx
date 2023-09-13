47c92a1441993a20a3dff1b606c4f83f69eef7d8
sfia-o <sofia.vl.oliveira@gmail.com>
2023-09-13 11:58:00 -0700

    Removing sample data and PhotoListItem

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,44 +1,14 @@
 import React from "react";
-import PhotoFavButton from './components/PhotoFavButton';
+import PhotoList from './components/PhotoList'
 import PhotoListItem from "./components/PhotoListItem";
 import "./App.scss";
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-// Note: Rendering a single component to build components in isolation
 const App = () => {
+
   return (
     <div className="App">
-      
-      <PhotoFavButton className="photo-list__fav-icon"/>
-      <PhotoListItem
-        className="photo-list__item"
-        data={sampleDataForPhotoListItem}
-        key={sampleDataForPhotoListItem.id}
-      />
-
-      <PhotoFavButton className="photo-list__fav-icon"/>
-      <PhotoListItem
-        className="photo-list__item"
-        data={sampleDataForPhotoListItem}
-        key={sampleDataForPhotoListItem.id}
-      />
-
-      <PhotoFavButton className="photo-list__fav-icon"/>
-      <PhotoListItem
-        className="photo-list__item"
-        data={sampleDataForPhotoListItem}
-        key={sampleDataForPhotoListItem.id}
-      />
+      {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
+      <PhotoList/>
     </div>
   );
 };
