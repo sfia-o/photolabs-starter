805c27d3f52fcf0ffaf416ba879f9d42373d9bd5
sfia-o <sofia.vl.oliveira@gmail.com>
2023-09-13 11:58:17 -0700

    Implement handlelick to like photo

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,12 +4,21 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
+
+  const [liked, setLike] = useState(false)
+
+  const handleClick =useCallback(() => {
+    setLike((prev) => !prev);
+  }, []); 
+
   return (
-    <div className="photo-list__fav-icon">
+    
+    <div onClick={handleClick} className="photo-list__fav-icon" >
       <div className="photo-list__fav-icon-svg">
-        <FavIcon />
+        <FavIcon selected={liked} />
       </div>
     </div>
+    
   );
 }
 
