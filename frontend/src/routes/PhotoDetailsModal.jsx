0a3e38ea128afbf1d140173a5d4fca6a9f67623e
sfia-o <sofia.vl.oliveira@gmail.com>
2023-09-16 10:55:58 -0700

    Pass closeModal onClick event

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -3,12 +3,15 @@ import React from 'react';
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
-const PhotoDetailsModal = () => {
+const PhotoDetailsModal = ({closeModal}) => {
+  const handleClick = () => {
+    closeModal();
+  }
 
 
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button">
+      <button onClick={handleClick} className="photo-details-modal__close-button">
         <img src={closeSymbol} alt="close symbol" />
       </button>
     </div>
