b1df986b8df64540d78f80f55682892429e7c51c
sfia-o <sofia.vl.oliveira@gmail.com>
2023-09-16 10:51:39 -0700

    Add modal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import {useState} from 'react';
 import HomeRoute from './routes/HomeRoute';
+import PhotoDetailsModal from './routes/PhotoDetailsModal';
 
 import "./App.scss";
 
 const App = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <div className="App">
       <HomeRoute />
+      {open && <PhotoDetailsModal/>}
     </div>
   );
 };
