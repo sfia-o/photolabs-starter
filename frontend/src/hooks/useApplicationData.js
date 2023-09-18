405d43ca232508b1fb12c4fcd1bc10ed8dadf1fd
sfia-o <sofia.vl.oliveira@gmail.com>
2023-09-18 11:45:07 -0700

    Initial commit

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,42 +1,56 @@
-import { useState } from "react";
+import { useReducer } from "react";
 
-function useApplicationData() {
-  const [openModal, setOpenModal] = useState(false);
-  const [selectedPhoto, setSelectedPhoto] = useState(null);
-  const [favouritePhotos, setFavouritePhotos] = useState([]);
+const initialState = {
+  openModal: false,
+  selectedPhoto: null,
+  favouritePhotos: []
+};
 
-  const toggleFavourite = (photoId) => {
-    if (favouritePhotos.includes(photoId)) {
-      setFavouritePhotos(favouritePhotos.filter((id) => id !== photoId));
-    } else {
-      setFavouritePhotos([...favouritePhotos, photoId]);
-    }
-  };
+const ACTIONS = {
+  TOGGLE_MODAL: "TOGGLE_MODAL",
+  SET_SELECTED_PHOTO: "SET_SELECTED_PHOTO",
+  TOGGLE_FAVOURITE: "TOGGLE_FAVOURITE"
+};
 
-  const isFavourite = (photoId) => {
-    return favouritePhotos.includes(photoId);
-  };
-
-  const hasFavourites = favouritePhotos.length > 0;
-
-  // Function to open the modal
-  const toggleModal = (photoData) => {
-    setOpenModal(!openModal);
-    setSelectedPhoto(photoData);
-  };
-
-  const closeModal = () => {
-    setOpenModal(false);
-  };
-
-  return {
-    state: { openModal, selectedPhoto, favouritePhotos },
-    toggleFavourite,
-    isFavourite,
-    hasFavourites,
-    toggleModal,
-    closeModal,
-  };
+function reducer(state, action) {
+  
 }
+
+
+
+
+//   const toggleFavourite = (photoId) => {
+//     if (favouritePhotos.includes(photoId)) {
+//       setFavouritePhotos(favouritePhotos.filter((id) => id !== photoId));
+//     } else {
+//       setFavouritePhotos([...favouritePhotos, photoId]);
+//     }
+//   };
+
+//   const isFavourite = (photoId) => {
+//     return favouritePhotos.includes(photoId);
+//   };
+
+//   const hasFavourites = favouritePhotos.length > 0;
+
+//   // Function to open the modal
+//   const toggleModal = (photoData) => {
+//     setOpenModal(!openModal);
+//     setSelectedPhoto(photoData);
+//   };
+
+//   const closeModal = () => {
+//     setOpenModal(false);
+//   };
+
+//   return {
+//     state: { openModal, selectedPhoto, favouritePhotos },
+//     toggleFavourite,
+//     isFavourite,
+//     hasFavourites,
+//     toggleModal,
+//     closeModal,
+//   };
+// }
 
 export default useApplicationData;
