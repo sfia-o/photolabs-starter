8269af6bda64364e2e0af301f7a97183237c8441
sfia-o <sofia.vl.oliveira@gmail.com>
2023-09-18 13:17:03 -0700

    Implement reducer

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -3,54 +3,78 @@ import { useReducer } from "react";
 const initialState = {
   openModal: false,
   selectedPhoto: null,
-  favouritePhotos: []
+  favouritePhotos: [],
 };
 
 const ACTIONS = {
   TOGGLE_MODAL: "TOGGLE_MODAL",
   SET_SELECTED_PHOTO: "SET_SELECTED_PHOTO",
-  TOGGLE_FAVOURITE: "TOGGLE_FAVOURITE"
+  TOGGLE_FAVOURITE: "TOGGLE_FAVOURITE",
 };
 
 function reducer(state, action) {
-  
+  switch (action.type) {
+    case ACTIONS.TOGGLE_MODAL:
+      return {
+        ...state,
+        openModal: !state.openModal,
+        selectedPhoto: action.payload
+      };
+    case ACTIONS.SET_SELECTED_PHOTO:
+      return {
+        ...state,
+        selectedPhoto: action.payload,
+      };
+    case ACTIONS.TOGGLE_FAVOURITE:
+      const photoId = action.payload;
+      if (state.favouritePhotos.includes(photoId)) {
+        return {
+          ...state,
+          favouritePhotos: state.favouritePhotos.filter((id) => id !== photoId),
+        };
+      } else {
+        return {
+          ...state,
+          favouritePhotos: [...state.favouritePhotos, photoId],
+        };
+      }
+
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
+  }
 }
 
+function useApplicationData() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const hasFavourites = state.favouritePhotos.length > 0;
 
+  const toggleFavourite = (photoId) => {
+    dispatch({ type: ACTIONS.TOGGLE_FAVOURITE, payload: { photoId } });
+  };
 
+  const isFavourite = (photoId) => {
+    return state.favouritePhotos.includes(photoId);
+  };
 
-//   const toggleFavourite = (photoId) => {
-//     if (favouritePhotos.includes(photoId)) {
-//       setFavouritePhotos(favouritePhotos.filter((id) => id !== photoId));
-//     } else {
-//       setFavouritePhotos([...favouritePhotos, photoId]);
-//     }
-//   };
+  const toggleModal = (photoData) => {
+    dispatch({ type: ACTIONS.TOGGLE_MODAL });
+    dispatch({ type: ACTIONS.SET_SELECTED_PHOTO, payload: photoData });
+  };
 
-//   const isFavourite = (photoId) => {
-//     return favouritePhotos.includes(photoId);
-//   };
+  const closeModal = () => {
+    dispatch({ type: ACTIONS.TOGGLE_MODAL });
+  };
 
-//   const hasFavourites = favouritePhotos.length > 0;
-
-//   // Function to open the modal
-//   const toggleModal = (photoData) => {
-//     setOpenModal(!openModal);
-//     setSelectedPhoto(photoData);
-//   };
-
-//   const closeModal = () => {
-//     setOpenModal(false);
-//   };
-
-//   return {
-//     state: { openModal, selectedPhoto, favouritePhotos },
-//     toggleFavourite,
-//     isFavourite,
-//     hasFavourites,
-//     toggleModal,
-//     closeModal,
-//   };
-// }
+  return {
+    state,
+    toggleFavourite,
+    isFavourite,
+    hasFavourites,
+    toggleModal,
+    closeModal,
+  };
+}
 
 export default useApplicationData;
